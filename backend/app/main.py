0536f69286db61b71dd00c7adc9ab187278e173b
IT24101458-M.G.M. Sanvidu <165678028+ManithSanvidu@users.noreply.github.com>
2026-09-01 20:22:24 +0530

    feat: added video uploading and analyzing road damages

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
 from app.database.database import Base, engine
-from app.routers import auth_router, upload_router
+from app.routers import auth_router, upload_router, video_router
 
 Base.metadata.create_all(bind=engine)
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI(title="Road Damage Detection API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router.router)
 app.include_router(upload_router.router)
+app.include_router(video_router.router)
 
 
 @app.get("/")
